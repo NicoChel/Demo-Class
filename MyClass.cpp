9c1a03a88ecae82024b5f4a61cb95e252fc2a7e7
NicoChel <ncheltenham@users.noreply.github.com>
2016-09-29 14:04:05 -0400

    Filled in missing parts

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,1 +1,31 @@
 // Implementation File
+
+void MyClass::setNum(int v)
+{
+	num = v;
+}
+
+int MyClass::getNum
+{
+	return num;
+}
+
+void MyClass::setRoom(string w)
+{
+	room = w;
+}
+
+string MyClass::getRoom
+{
+	return room;
+}
+
+void MyClass::setFun(bool x)
+{
+	fun = x;
+}
+
+bool MyClass::getFun
+{
+	return x;
+}
